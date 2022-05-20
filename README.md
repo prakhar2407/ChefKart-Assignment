0a7970f825593cd6c8a88e55886477ef3c6ffd17
prakhar2407 <65302454+prakhar2407@users.noreply.github.com>
2022-05-20 17:22:18 +0530

    completed readme file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ChefKart-Assignment
+
+# To run the app
+
+Step-1: Clone the Project
+Step-2: Run the command (mvn spring-boot:run)
+
+For this to run you should have Java, Maven and both of them should be properly defined as MAVEN_HOME and JAVA_HOME
+in enviornment variables
+
+# Unit Testing Code
+
+It is present inside test folder
+
+Step-1: Run command (mvn test)
+or just by clicking the play button on left of vscode
--- src/test/java/com/prakhar/chefkart/APIControllerTest.java
+++ src/test/java/com/prakhar/chefkart/APIControllerTest.java
@@ -1,28 +1,26 @@
 package com.prakhar.chefkart;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.List;
 
 import com.prakhar.chefkart.controller.APIController;
 import com.prakhar.chefkart.models.Post;
 import com.prakhar.chefkart.service.serviceImpl.APIServiceImpl;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-// @RunWith(SpringRunner.class)
 @WebMvcTest(value = APIController.class)
 @WithMockUser
 public class APIControllerTest {
@@ -32,5 +30,18 @@ public class APIControllerTest {
     @MockBean
     private APIServiceImpl apiServiceImpl;
     Post post = new Post(1, "Prakhar", "This is my name", 215, new String[] { "URL1", "URL2" });
+
+    @Test
+    public void retrieveCourse() throws Exception {
+        Mockito.when(apiServiceImpl.getAllPost()).thenReturn((List<Post>) post);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/allPosts").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        System.out.println(result.getResponse());
+
+        assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+    }
 
 }
